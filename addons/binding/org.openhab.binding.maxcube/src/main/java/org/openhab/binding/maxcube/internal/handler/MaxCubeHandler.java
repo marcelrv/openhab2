8941153954f998cef626adfc37da847252510e20
marcel <marcel@verpaalen.com>
2014-09-10 10:52:35 +0200

    Improving of MaxCubeHandler

--- addons/binding/org.openhab.binding.maxcube/src/main/java/org/openhab/binding/maxcube/internal/handler/MaxCubeHandler.java
+++ addons/binding/org.openhab.binding.maxcube/src/main/java/org/openhab/binding/maxcube/internal/handler/MaxCubeHandler.java
@@ -11,16 +11,21 @@ import static org.openhab.binding.maxcube.MaxCubeBinding.CHANNEL_BATTERY;
 import static org.openhab.binding.maxcube.MaxCubeBinding.CHANNEL_SETTEMP;
 import static org.openhab.binding.maxcube.MaxCubeBinding.CHANNEL_VALVE;
 
+import java.util.ArrayList;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 import org.eclipse.smarthome.config.core.Configuration;
 import org.eclipse.smarthome.core.library.types.DecimalType;
+import org.eclipse.smarthome.core.thing.Bridge;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
 import org.eclipse.smarthome.core.types.Command;
 import org.eclipse.smarthome.core.types.State;
+import org.openhab.binding.maxcube.config.MaxCubeConfiguration;
+import org.openhab.binding.maxcube.internal.message.Device;
 import org.openhab.core.library.types.StringType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,57 +39,83 @@ import org.slf4j.LoggerFactory;
 public class MaxCubeHandler extends BaseThingHandler {
 
 	private String serialnr;
-    private Logger logger = LoggerFactory.getLogger(MaxCubeHandler.class);
-    private int refresh = 60; // refresh every minute as default 
-    ScheduledFuture<?> refreshJob;
-    
+	private Logger logger = LoggerFactory.getLogger(MaxCubeHandler.class);
+	private int refresh = 60; // refresh every minute as default 
+	ScheduledFuture<?> refreshJob;
+	private MaxCubeBridgeHandler bridgeHandler;
+
+	private Device maxCubeDevice;	
+	private String maxCubeDeviceSerial;
+
 	public MaxCubeHandler(Thing thing) {
 		super(thing);
 	}
 
 	@Override
-    public void initialize() {
-        logger.debug("Initializing Maxcube handler.");
+	public void initialize() {
+		final String configDeviceId = getConfigAs(MaxCubeConfiguration.class).serialNumber;
+		if (configDeviceId != null) {
+			maxCubeDeviceSerial = configDeviceId;
+		}
+		if (maxCubeDeviceSerial != null){
+			logger.debug("Initialized maxcube device handler for {}.", maxCubeDeviceSerial);}
+		else {
+			logger.debug("Initialized maxcube device missing serialNumber configuration... troubles ahead");
+		}
 
-        Configuration config = getThing().getConfiguration();
+		//testing only: fake updates
+		startAutomaticRefresh();
+	}
 
-        serialnr = (String) config.get("serial");
-        logger.debug("Item Serial,{}.", serialnr);
-        
-        try {
-        	refresh = Integer.parseInt((String)config.get("refresh"));
-        } catch(Exception e) {
-        	// let's ignore it and go for the default
-        }
-        
-        startAutomaticRefresh();
-    }
-	
-	
-private void startAutomaticRefresh() {
-		
+	private void startAutomaticRefresh() {
+
 		Runnable runnable = new Runnable() {
 			public void run() {
 				try {
 					//updateWeatherData();
-	                updateState(new ChannelUID(getThing().getUID(), CHANNEL_VALVE), (State) new DecimalType("15"));
-	                updateState(new ChannelUID(getThing().getUID(), CHANNEL_BATTERY), (State) new DecimalType("10") );
-	                updateState(new ChannelUID(getThing().getUID(), CHANNEL_SETTEMP), (State) new DecimalType("125"));
+					updateState(new ChannelUID(getThing().getUID(), CHANNEL_VALVE), (State) new DecimalType("15"));
+					updateState(new ChannelUID(getThing().getUID(), CHANNEL_BATTERY), (State) new DecimalType("10") );
+					updateState(new ChannelUID(getThing().getUID(), CHANNEL_SETTEMP), (State) new DecimalType("125"));
 				} catch(Exception e) {
 					logger.debug("Exception occurred during execution: {}", e.getMessage(), e);
 				}
 			}
 		};
-		
+
 		refreshJob = scheduler.scheduleAtFixedRate(runnable, 0, refresh, TimeUnit.SECONDS);
 	}
 
 
+	private synchronized MaxCubeBridgeHandler getMaxCubeBridgeHandler() {
+		if(this.bridgeHandler==null) {
+			Bridge bridge = getBridge();
+			if (bridge == null) {
+				return null;
+			}
+			ThingHandler handler = bridge.getHandler();
+			if (handler instanceof MaxCubeBridgeHandler) {
+				this.bridgeHandler = (MaxCubeBridgeHandler) handler;
+				//        	this.bridgeHandler.registerDeviceStatusListener(this);
+			} else {
+				return null;
+			}
+		}
+		return this.bridgeHandler;
+	}
 
 	@Override
 	public void handleCommand(ChannelUID channelUID, Command command) {
-        if(channelUID.getId().equals(CHANNEL_SETTEMP)) {
-            // TODO: handle command
-        }
+		MaxCubeBridgeHandler maxCubeBridge = getMaxCubeBridgeHandler();
+		if (maxCubeBridge == null) {
+			logger.warn("maxCube LAN gateway bridge handler not found. Cannot handle command without bridge.");
+			return;
+		}
+
+		if(channelUID.getId().equals(CHANNEL_SETTEMP)) {
+			logger.warn("Setting of temp not implemented.");
+		}
+		else {
+			logger.warn("Setting of channel {} not possible. Read-only", channelUID);
+		}
 	}
 }
